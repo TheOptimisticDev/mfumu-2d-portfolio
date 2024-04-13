1f732dadbd4af2427598ec098726bdd03f054ae9
OptimisticUnderdog <mabunda.wealth@gmail.com>
2024-04-13 10:45:21 +0200

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,30 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my mini-lab 🧪, where I cook up code concoctions! I'm all about JavaScript/TypeScript, diving deep into software development. With React, HTML, CSS, and a sprinkle of Python 🐍, I've whipped up some pretty neat apps!😅 This portfolio was developed here!!💻🔥 But hey, I'm not just a one-trick pony. 
+  I'm also cozying up to Dart and exploring more Python 🐍 adventures since I'm currently completing my AWS re/Start Program. 
+  Through this program, I've gained invaluable insights into AWS services like EC2, S3, Lambda, DynamoDB, RDS, IAM, and CloudWatch, enhancing my understanding of cloud computing. Python, alongside Bash and Linux, is a key component in the cloud landscape, further motivating me to excel in both software development and cloud technology.
+  Regardless of the language, I'm all about programming and cooking up innovative solutions. So, feel free to dig into my projects on <a href="https://github.com/TheOptimisticDev" target="_blank">GitHub</a>! 🚀🛠️💻🔥`,
+  "se-certificate": `This is my Software Engineering certificate proudly displayed on my wall 📜. I specialized in Front-end Development, while it provided both solid theoretical and practical hands on foundational tasks, I value it as a testament to my dedication and hard work in pursuing my passion for technology 💻. 
+  Verify it on <a href="https://intranet.alxswe.com/certificates/78m9CpLcfX" target="_blank">ALX</a>! 🔍🌟`,
+  "sofa-table": `That's my comfy spot on the sofa, where I unwind by catching up on my favorite TV shows or diving into a good book (Data Structures & Algorithms in JavaScript). 🛋️📺📚 It's my haven for relaxation and rejuvenation after a long day. Sometimes, I'll even indulge in a bit of gaming to unwind and let off some steam. 💻🕹️`,
+  tv: `That's my TV 📺. I often use it to watch online lessons on current technologies provided by various YouTubers 📚💻, also this is where i learnt how to build this awesome interactive portfolio, check out the tutorial <a href="https://www.youtube.com/watch?v=wy_fSStEgMs" target="_blank">here!!</a>, 
+  special thanks to <a href="https://github.com/JSLegendDev" target="_blank">JSLegendDev</a> and <a href="https://www.freecodecamp.org/news/create-a-developer-portfolio-as-a-2d-game/" target="_blank">FreeCodeCamp</a> for a detailed and informative tutorial on Kaboom.js framework!!💻🔥`,
+  bed: `This bed 🛏️ is like my idea hub, where brilliance strikes when I least expect it! When inspiration decides to crash the slumber party, I've got to play the part of the frantic secretary, scribbling down those genius notions ✍️. 
+  Otherwise, they'll bounce around my brain all night like hyperactive monkeys, turning my peaceful sleep into a chaotic circus! 
+  😴 And let me tell you, chasing down those elusive thoughts while half-asleep is a game I'd rather snooze through!`,
+  resume: `This is my desk 🖥️, and on it is my resume. Feel free to check it out on my <a href="https://drive.google.com/file/d/1izrWEcb96MZ-88TNGDcjwknwzBwYMGIv/view?usp=drive_link" target="_blank">Google Drive</a>! 
+  Contact me at <a href="mailto:mabunda.wealth@gmail.com">mabunda.wealth@gmail.com</a>, if you have any interesting job opportunities or looking for a collaboration!📧👨‍💼 or maybe chat over a cup of<a href="https://www.buymeacoffee.com/mabundawealth" target="_blank"> coffee☕!!</a>`,
+  projects: `
+  Confidential Intel 🕵️‍♂️: This creation utilizes the Kaboom.js library, an enigmatic tool for crafting games in JavaScript 🎮. Stealthy maneuvers are employed to render text with HTML/CSS, ensuring that the message you're decoding remains hidden outside the canvas 🖌️. 
+  Visit my <a href="https://github.com/TheOptimisticDev" target="_blank">GitHub</a> profile to get your hands on the source code while it still lasts, because it WILL last forever! No seriously it will last forever! 😅🔒`,
+  library: `This is my personal inventory. Here, you'll find quite a collection of programming books on my shelves 📚. Admittedly, I've probably only finished reading one of them 📖. Who else can relate to compulsively buying technical books without ever getting through them all? 😅🤷‍♂️ 
+  While you here please check out my digital badges: <a href="https://www.credly.com/badges/13c2abe1-2d39-4bef-8b26-cf2bf3fdb5f9/public_url" target="_blank">AWS Cloud Quest | Cloud Practitioner Fundamentals</a>, 
+  <a href="https://www.credly.com/badges/56da5e07-6f2d-4480-8fee-f41cce934d70/public_url" target="_blank">AWS Cloud Essential</a>, 
+  <a href="https://learn.microsoft.com/api/achievements/share/en-us/MfumuWealthMabunda-4581/DG4LZM7J?sharingId=F561C7C57E3D99CF" target="_blank">Microsoft Azure Fundamentals</a>. Oh! there is plenty more, trophys too!!🚀, 
+  <a href="https://learn.microsoft.com/api/achievements/share/en-us/MfumuWealthMabunda-4581/YVNVA79R?sharingId=F561C7C57E3D99CF" target="_blank">Microsoft Azure Architect Design</a>, 
+  <a href="https://learn.microsoft.com/api/achievements/share/en-us/MfumuWealthMabunda-4581/UF6NCFY3?sharingId=F561C7C57E3D99CF" target="_blank">Microsoft Azure architecture and services</a>, 
+  <a href="https://learn.microsoft.com/api/achievements/share/en-us/MfumuWealthMabunda-4581/J6KGEDST?sharingId=F561C7C57E3D99CF" target="_blank">Microsoft Azure management and governance</a>. More are brewing!!📚💻🖌️`,
+  exit: `
+  If you're feeling captured in TheOptimisticDev's portfolio, don't worry! Just close the tab👋 and you're free to roam the wilds of the internet once again 🌐. No need to send a search party! But do check out my inventory before you leave😅🚀🔍`,
+};
